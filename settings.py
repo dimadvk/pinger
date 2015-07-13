2a2c117545cf53316905405fda9c1b89d7f4456e
Dimitriy V. Kutnyakov <d.v.kutnyakov@gmail.com>
2015-07-13 14:52:50 +0300

    remove ----- from fate list

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Name of database for storing data.
+# Name of database for storing data. Don't change if it not neccessary.
 db_name='pinger_db.sqlite3'
 
 # Path to ping utility
