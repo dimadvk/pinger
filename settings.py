b28bb6feb28879305f5f3ee5e2c3817c7df9bef3
Dimitriy V. Kutnyakov <d.v.kutnyakov@gmail.com>
2015-10-05 13:22:34 +0300

    puth there global variables "warning_packetloss_level1(2)"

--- settings.py
+++ settings.py
@@ -8,3 +8,11 @@ ping = '/bin/ping'
 
 # Monitoring results older then "days_results_obselete" will be removed from base
 day_results_obselete = '30'
+
+# Set scopes of warning level. Color of row in monitoring results depends on that.
+# if warning_packetloss_level1 < packet_loss < warning_packetloss_level2
+#	than light_red line color
+# if packet_loss > warning_packetloss_level2
+#   than red line color
+warning_packetloss_level1 = 0
+warning_packetloss_level2 = 6
