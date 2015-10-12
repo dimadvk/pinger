511ec0aa3608d9ed2abb13f6c62aa49c06a43732
Dimitriy Kutnyakov <ren-kpi@i.ua>
2015-10-12 19:12:12 +0300

    formatting schema.sql

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE group_list
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-     group_name text,
-     group_comment text);
+     group_name TEXT,
+     group_comment TEXT);
 CREATE TABLE ip_list
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-     ip text,
-     hostname text,
-     group_id integer,
+     ip TEXT,
+     hostname TEXT,
+     group_id INTEGER,
      FOREIGN KEY (group_id) REFERENCES group_list(id));
 CREATE TABLE ping_results
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-     date_time text,
-     ip text,
-     sent text,
-     received text)
+     date_time TEXT,
+     ip TEXT,
+     sent TEXT,
+     received TEXT)
 
